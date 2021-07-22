aae2c688a3066ac6845cc5b3a6965078eb2ea4fb
anurag3440 <38152632+anurag3440@users.noreply.github.com>
2021-07-22 16:06:06 +0530

    alert notification

---
+++ Alert System/alert_notification.py
@@ -0,0 +1,21 @@
+import datetime  # for reading present date
+import time
+import requests  # for retreiving coronavirus data from web
+from plyer import notification  # for getting notification on your PC
+
+
+condition = 1  # for testing the notification only
+def notifyMe(title,message):
+    notification.notify(
+        title=title,
+        message=message,
+        #creating icon for the notification
+        #we need to give adress of icon file(in ico file format)
+        app_icon="\\icon.ico",
+        # the notification stays for 50sec
+        timeout=15
+    )
+
+#if we detect person without mask
+if (condition):
+    notifyMe("Mask Detection Alert!","Someone is without mask")
